fed0b2db254bd977987f6d956c84e637adc48b01
Utshav pandey <utsavpandey8@gmail.com>
2020-11-19 23:47:28 +0530

    refresh__feature

--- Clone/App.js
+++ Clone/App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 // other imports
 import {NavigationContainer} from '@react-navigation/native';
 
-import React, {useEffect, useCallback, useState} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -22,29 +22,9 @@ const Stack = createStackNavigator();
 
 const App = () => {
   // console.warn('hiiii CLone here!');
-  const [color, setColor] = useState([]);
-  const colorHandler = useCallback(async () => {
-    const result = await fetch(
-      'https://color-palette-api.kadikraman.now.sh/palettes',
-    );
-    if (result.ok) {
-      const colors = await result.json();
-      setColor(colors);
-    }
-    console.warn(result.json);
-  });
-
-  useEffect(() => {
-    colorHandler();
-  }, []);
   return (
     <>
-      <FlatList
-        data={color}
-        keyExtractor={(item) => item.hexCode}
-        renderItem={({item}) => <Text>{item}</Text>}
-      />
-      {/* <NavigationContainer>
+      <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen
@@ -53,7 +33,7 @@ const App = () => {
             options={({route}) => ({title: route.params.paletteName})}
           />
         </Stack.Navigator>
-      </NavigationContainer> */}
+      </NavigationContainer>
     </>
   );
 };
--- Clone/screens/Home.js
+++ Clone/screens/Home.js
@@ -1,55 +1,44 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useEffect, useCallback, useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  RefreshControl,
+} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import PalettePreview from '../components/PalettePreview';
 
-const SOLARIZED = [
-  {colorName: 'Base03', hexCode: '#002b36'},
-  {colorName: 'Base02', hexCode: '#073642'},
-  {colorName: 'Base01', hexCode: '#586e75'},
-  {colorName: 'Base00', hexCode: '#657b83'},
-  {colorName: 'Base0', hexCode: '#839496'},
-  {colorName: 'Base1', hexCode: '#93a1a1'},
-  {colorName: 'Base2', hexCode: '#eee8d5'},
-  {colorName: 'Base3', hexCode: '#fdf6e3'},
-  {colorName: 'Yellow', hexCode: '#b58900'},
-  {colorName: 'Orange', hexCode: '#cb4b16'},
-  {colorName: 'Red', hexCode: '#dc322f'},
-  {colorName: 'Magenta', hexCode: '#d33682'},
-  {colorName: 'Violet', hexCode: '#6c71c4'},
-  {colorName: 'Blue', hexCode: '#268bd2'},
-  {colorName: 'Cyan', hexCode: '#2aa198'},
-  {colorName: 'Green', hexCode: '#859900'},
-];
-
-const RAINBOW = [
-  {colorName: 'Red', hexCode: '#FF0000'},
-  {colorName: 'Orange', hexCode: '#FF7F00'},
-  {colorName: 'Yellow', hexCode: '#FFFF00'},
-  {colorName: 'Green', hexCode: '#00FF00'},
-  {colorName: 'Violet', hexCode: '#8B00FF'},
-];
-
-const FRONTEND_MASTERS = [
-  {colorName: 'Red', hexCode: '#c02d28'},
-  {colorName: 'Black', hexCode: '#3e3e3e'},
-  {colorName: 'Grey', hexCode: '#8a8a8a'},
-  {colorName: 'White', hexCode: '#ffffff'},
-  {colorName: 'Orange', hexCode: '#e66225'},
-];
-
-const COLOR_PALETTES = [
-  {paletteName: 'Solarized', colors: SOLARIZED},
-  {paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS},
-  {paletteName: 'Rainbow', colors: RAINBOW},
-];
-
 const Home = ({navigation}) => {
+  const [color, setColor] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const colorHandler = useCallback(async () => {
+    const result = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+    if (result.ok) {
+      const colors = await result.json();
+      setColor(colors);
+    }
+  });
+
+  useEffect(() => {
+    colorHandler();
+  }, []);
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await setColor();
+    setTimeout(() => {
+      setColor(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <FlatList
         style={styles.list}
-        data={COLOR_PALETTES}
+        data={color}
         // eslint-disable
         keyExtractor={(item) => item.paletteName}
         renderItem={({item}) => (
@@ -58,6 +47,11 @@ const Home = ({navigation}) => {
             colorPalette={item}
           />
         )}
+        refreshing={isRefreshing}
+        onRefresh={handleRefresh}
+        // refreshControl={
+        //   <RefreshControl refreshing={true} onRefresh={() => {}} />
+        // }
       />
     </>
   );
